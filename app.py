e8619d278eba6273b554482410f99814b8d005a4
Potamochoerus <github.com.croak130@passmail.com>
2026-01-31 14:42:41 +0100

    Read from Bakkesmod logs

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from faicons import icon_svg
 import numpy as np
 
 # Import data from shared.py
-from shared import app_dir, match_history, threshold_score
+from shared import app_dir, match_history, threshold_score, tracked_players, total_games, participation_dictionary
 
 # Import shiny
 from shiny import reactive
@@ -13,13 +13,23 @@ from shiny.express import input, render, ui
 ui.page_opts(title="SamuTracker", fillable=True)
 
 with ui.sidebar(title="Filter games"):
-    ui.input_slider("n_games", "Number of games to use", 1, min(100, match_history.shape[0]), min(100, match_history.shape[0]))
+    ui.input_select(id="mode",
+                    label="Game mode",
+                    choices=["3v3", "2v2", "1v1"])
+    
     ui.input_checkbox_group(
-        "players",
-        "Players",
-        ["Jayke Stew", "Pongo Abeii", "Palaindrome"],
-        selected=["Jayke Stew", "Pongo Abeii", "Palaindrome"],
+        id="players",
+        label="Players",
+        choices=tracked_players,
+        selected=list(tracked_players.keys()),
     )
+
+    ui.input_slider(id="n_games", 
+                    label="Number of games to use", 
+                    min=1, 
+                    max=min(100, total_games), 
+                    value=min(100, total_games),
+                    step=1)
 
 
 with ui.layout_columns():
@@ -28,27 +38,45 @@ with ui.layout_columns():
 
         @render.plot
         def barplot_mean():
-            return sns.barplot(
-                data=filtered_mh_longer(),
-                x="players",
-                y="score",
-                hue="players",
+            bp = sns.barplot(
+                data=filtered_mh(),
+                x="FixedName",
+                y="Score",
+                hue="FixedName",
                 errorbar=None
             )
+            for p in bp.patches:
+                height = p.get_height()  # height of the bar
+                bp.text(
+                    x=p.get_x() + p.get_width() / 2,  # center of bar
+                    y=height + 0.3,                    # slightly above bar
+                    s=f"{int(height)}",                # text to display
+                    ha='center'                        # horizontal alignment
+                )
+            return bp
 
     with ui.card(full_screen=True):
         ui.card_header("Median score")
 
         @render.plot
         def barplot_median():
-            return sns.barplot(
-                data=filtered_mh_longer(),
-                x="players",
-                y="score",
-                hue="players",
+            bp = sns.barplot(
+                data=filtered_mh(),
+                x="FixedName",
+                y="Score",
+                hue="FixedName",
                 estimator=np.median,
                 errorbar=None
             )
+            for p in bp.patches:
+                height = p.get_height()  # height of the bar
+                bp.text(
+                    x=p.get_x() + p.get_width() / 2,  # center of bar
+                    y=height + 0.3,                    # slightly above bar
+                    s=f"{int(height)}",                # text to display
+                    ha='center'                        # horizontal alignment
+                )
+            return bp
 
 
 with ui.layout_columns():
@@ -57,18 +85,18 @@ with ui.layout_columns():
 
         @render.plot
         def n_below_barplot():
-            df = filtered_mh_longer()
-            df["below_threshold"] = df["score"] < threshold_score
+            df = filtered_mh()
+            df["below_threshold"] = df["Score"] < threshold_score
 
             # Count per player
-            count_df = df.groupby("players")["below_threshold"].sum().reset_index()
+            count_df = df.groupby("FixedName")["below_threshold"].sum().reset_index()
             count_df.rename(columns={"below_threshold": f"count_below_{threshold_score}"}, inplace=True)
 
             return sns.barplot(
-                x="players",
+                x="FixedName",
                 y=f"count_below_{threshold_score}",
                 data=count_df,
-                hue="players",
+                hue="FixedName",
                 palette="pastel"
             )
 
@@ -99,19 +127,15 @@ ui.include_css(app_dir / "styles.css")
 @reactive.calc
 def filtered_mh():
     player_list = list(input.players())
-    player_list.append("game_id")
-    filt_mh = match_history[player_list]
-    filt_mh = filt_mh.head(input.n_games())
+    # First, filter by game mode
+    filt_mh = match_history[match_history["GameMode"]==input.mode()]
+    # Then get only games where all selected players are present
+    games_with_all_selected_players = [k for k, v in participation_dictionary.items() if all(x in v for x in input.players())]
+    filt_mh = filt_mh[filt_mh["Timestamp"].isin(games_with_all_selected_players)]
+    # Keep only entries of players of interest
+    filt_mh = filt_mh[filt_mh["AccountId"].isin(player_list)]
+    # Finally select n games.
+    unique_games = list(set(filt_mh.Timestamp))
+    games_selected = sorted(unique_games, reverse=True)[0:input.n_games()]
+    filt_mh = filt_mh[filt_mh.Timestamp.isin(games_selected)]
     return filt_mh
-
-@reactive.calc
-def filtered_mh_longer():
-    df = filtered_mh()
-    df_long = df.melt(
-        id_vars=["game_id"],
-        value_vars=list(input.players()),
-        var_name="players",
-        value_name="score"
-    )
-    return df_long
-
--- shared.py
+++ shared.py
@@ -1,6 +1,64 @@
 from pathlib import Path
 import pandas as pd
+import glob
+import yaml
 
 app_dir = Path(__file__).parent
-match_history = pd.read_csv(app_dir / "data" / "match_history.csv")
 threshold_score = 100
+with open("tracked_players.yml", "r") as f:
+    tracked_players = yaml.safe_load(f)
+raw_data_path = app_dir / "data"
+
+# Read all matches
+def read_history(data_path):
+    """
+    Read all match history and parse it in a readable way for the app
+
+    :param data_path: Folder where the exported .csv from Bakkesmod are stored
+    """
+    all_matches = glob.glob(str(data_path / "*.csv"))
+    match_history = []
+    for match in all_matches:
+        df_match = pd.read_csv(match, index_col=None, header=0)
+        match_history.append(df_match)
+
+    # Regroup all matches
+    match_history = pd.concat(match_history, axis=0, ignore_index=True)
+    
+    
+    # Add date and time
+    match_history[["Date", "Time"]] = match_history["Timestamp"].str.split("_", expand = True)
+
+    # Deduplicate in case the same game is added twice
+    match_history = match_history.drop_duplicates()
+
+    # For now bakkesmod does not log these for every player
+    match_history = match_history.drop(columns = ["Pads", "Boosts", "BoostUsage"])
+
+    # Add game mode
+    game_mode = match_history["Timestamp"].value_counts().reset_index()
+    game_mode.columns = ["Timestamp", "n_players"]
+    game_mode["GameMode"] = game_mode["n_players"].apply(lambda x: f"{int(x/2)}v{int(x/2)}")
+    game_mode = game_mode.drop(columns = ["n_players"])
+    match_history = pd.merge(match_history, game_mode, on="Timestamp", how="left")
+    
+    # Select players of interest
+    match_history = match_history[(match_history.AccountId.isin(tracked_players.keys()))]
+
+    # Add Fixed name
+    match_history["FixedName"] = match_history["AccountId"].map(tracked_players)
+
+    return match_history
+
+def participation_dict(match_history):
+    """
+    Generate a participation dictionary containing the list of players in each game
+    
+    :param match_history: Description
+    """
+    match_dict = match_history.groupby("Timestamp")["AccountId"].apply(list).to_dict()
+    return(match_dict)
+
+match_history = read_history(raw_data_path)
+total_games = len(list(set(match_history.Timestamp)))
+participation_dictionary = participation_dict(match_history)
